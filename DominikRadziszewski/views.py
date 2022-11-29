ae63535672ee8843ab36441cb55739b5cdb8a53d
DoradR <dominik.radziszewski99@gmail.com>
2022-11-29 22:06:20 +0100

    Create first look on projects_page.html

--- DominikRadziszewski/views.py
+++ DominikRadziszewski/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Contact, Post, PicturesOfPost
 
 
@@ -9,10 +9,20 @@ def front_page(request):
 
 def projects_page(request):
     posts = Post.objects.all()
-    pictures = PicturesOfPost.objects.order_by('whichPost')
+    pictures = PicturesOfPost.objects.all()
     return render(request, 'projects_page.html', {'posts': posts, 'pictures': pictures})
 
 
 def contact_page(request):
     contact = Contact.objects.all()
     return render(request, 'contact_page.html', {'contact': contact})
+
+
+def details_of_project_page(request, id):
+    post = get_object_or_404(Post, pk=id)
+    picturesOfProject = PicturesOfPost.objects.filter(whichPost=post)
+
+    if request.method == "POST":
+        post.objects.filter(id)
+
+    return render(request, 'details_of_project_page.html', {'post': post, 'picturesOfProject': picturesOfProject})
