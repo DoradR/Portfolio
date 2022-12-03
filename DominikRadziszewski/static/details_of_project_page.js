8fc5941e4b09a08103f94b9821b16df84596059f
DoradR <dominik.radziszewski99@gmail.com>
2022-12-03 20:48:21 +0100

    Making modal images works correctly

--- DominikRadziszewski/static/details_of_project_page.js
+++ DominikRadziszewski/static/details_of_project_page.js
@@ -1,16 +1,18 @@
 var modal = document.getElementById("myModal");
 
-var img = document.getElementById("myImg");
+var img = document.getElementsByClassName("myImg");
+for(var i=0; i<img.length; i++) {img[i].onclick = showCaption;}
 var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
+function showCaption(){
   modal.style.display = "block";
   modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
 }
 
 var span = document.getElementsByClassName("close")[0];
 
 span.onclick = function() {
+  modal.style.display = "none";
+}
+modal.onclick = function() {
   modal.style.display = "none";
 }
